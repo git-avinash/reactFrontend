fd4960525d94016524697fab52c156c786ea8abf
git-avinash <mail.avinashsah@gmail.com>
2020-09-15 16:02:15 +0530

    more updates

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import PrivateRoutes from "./auth/helper/PrivateRoutes";
 
 import Home from "./core/Home";
 
@@ -8,6 +9,7 @@ const Routes = () => {
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Home} />
+        {/* <PrivateRoutes path="user/dashboard" exact component={} /> */}
       </Switch>
     </BrowserRouter>
   );
